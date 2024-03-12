6d94abd5f368adab8e0db1a85a7c8aed70d8e85a
EbelAlfie <davis.onde.wei@gmail.com>
2024-03-12 23:01:51 +0700

    added controller

--- route/user/user_route.go
+++ route/user/user_route.go
@@ -2,10 +2,12 @@ package user
 
 import (
 	"github.com/gin-gonic/gin"
+
+	"wordly/controller"
 )
 
 func UserRoute(group *gin.RouterGroup) gin.HandlerFunc {
 	return gin.HandlerFunc(func(ctx *gin.Context) {
-		group.POST("/login")
+		group.POST("/login", controller.Login)
 	})
 }
