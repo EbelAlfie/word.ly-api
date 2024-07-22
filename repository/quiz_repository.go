06fef8fee1dd7f7aca6adcd6317740cb33b10b33
EbelAlfie <davis.onde.wei@gmail.com>
2024-07-22 23:22:35 +0700

    add db to quiz

--- repository/quiz_repository.go
+++ repository/quiz_repository.go
@@ -1,15 +1,21 @@
 package repository
 
 import (
+	"database/sql"
+
 	domain "wordly/api/domain"
 )
 
 type QuizRepositoryImpl struct {
 	//my sql
+	db *sql.DB
 }
 
 func CreateQuizRepository() domain.QuizRepository {
-	return &QuizRepositoryImpl{}
+	database := openSqlCon()
+	return &QuizRepositoryImpl{
+		db: database,
+	}
 }
 
 func (repo *QuizRepositoryImpl) GetQuizes() {
